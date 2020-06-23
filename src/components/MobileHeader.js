fee08ca77a93b4e11a86d4cbe9ea98882aad554f
Alyssa Jackson <alyssajackson@Alyssas-MacBook-Pro.local>
2020-06-22 19:53:51 -0600

    mobile dropdown menu

--- src/components/MobileHeader.js
+++ src/components/MobileHeader.js
@@ -5,16 +5,24 @@ import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
 class MobileHeader extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {dropdown: false}
+    }
     componentDidMount() {
         let sidenav = document.querySelector('#slide-out');
         M.Sidenav.init(sidenav, {});
-      }
+    }
     render() {
+        const dropDown = () => {
+            this.setState({dropdown: !this.state.dropdown})
+        }
         return(
         <nav id="nav">
-            <a href="#" data-target="slide-out" class="sidenav-trigger show-on-large"><i class="material-icons">menu</i><i class="fas fa-music"></i></a>
+            <a data-target="slide-out" className="sidenav-trigger show-on-large"><i className="material-icons">menu</i><i className="fas fa-music"></i></a>
+            <h4>Alyssa's Music Studio</h4>
             <div className="container">
-                <ul id="slide-out" class="sidenav">
+                <ul id="slide-out" className="sidenav">
                 <li>
                     <div className="user-view">
                     <div className="background">
@@ -27,7 +35,9 @@ class MobileHeader extends React.Component {
                 </li>
                     <li><Link to='/' className="sidenav-close">Home</Link></li>
                     <li><Link to='/about' className="sidenav-close">About</Link></li>
-                    <li><Link to='/lessons' className="sidenav-close">Lessons</Link></li>
+                    <li><a onClick={dropDown}className="lessons">Lessons<i className="fas fa-caret-down"/></a></li>
+                    <li className={this.state.dropdown? "show": "hidden"}><Link to='/lessons' className="sidenav-close" id="piano">Piano Lessons</Link></li>
+                    <li className={this.state.dropdown? "show": "hidden"}><Link to='/lessons' className="sidenav-close" id="guitar">Guitar Lessons</Link></li>
                     <li><Link to='/policies' className="sidenav-close">Policies</Link></li>
                     <li><Link to='/perform' className="sidenav-close">Perform</Link></li>
                     <li><Link to='/contact' className="sidenav-close">Contact</Link></li>
