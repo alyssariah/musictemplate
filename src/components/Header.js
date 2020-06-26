ab84c66c306e101dd98d71463bdff6a82a53f1e3
Alyssa Jackson <alyssajackson@Alyssas-MacBook-Pro.local>
2020-06-26 17:05:58 -0600

    more drop down css

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,25 +7,22 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 class Header extends React.Component {
     componentDidMount() {
         let elems = document.querySelectorAll('.dropdown-trigger');
-        M.Dropdown.init(elems, {inDuration: 300, outDuration: 225});
-        window.onscroll= function(){
-            let bar = document.getElementById('fullNav');
-            bar.classList.add("sticky")
-        }
-        }
+        M.Dropdown.init(elems, {inDuration: 0, outDuration: 0});
+    }
     render() {
         return(
         <>
             <h1 className="topHeader">Alyssa's Music Studio</h1>
-            <nav id="fullNav">
-                <ul id="dropdown1" class="dropdown-content">
+            <nav id="fullNav" className="sticky">
+                <ul id="dropdown1" className="dropdown-content">
+                    <Link to='/lessons'><a data-target="dropdown1">Lessons<i className="material-icons right">arrow_drop_down</i></a> </Link>
                     <li><Link to='/lessons' onClick={()=>this.props.setInstrument("Piano")}>Piano Lessons</Link></li>
                     <li class="divider"></li>
                     <li><Link to='/lessons' onClick={()=>this.props.setInstrument("Guitar")}>Guitar Lessons</Link></li>
                 </ul>
                 <Link to='/'>Home</Link>
                 <Link to='/about'>About</Link>
-                <Link to='/lessons'><a class="dropdown-trigger" href="#!" data-target="dropdown1">Lessons<i className="material-icons right">arrow_drop_down</i></a> </Link>
+                <a class="dropdown-trigger" href="#!" data-target="dropdown1">Lessons<i className="material-icons right">arrow_drop_down</i></a>
                 <Link to='/policies'>Policies</Link>
                 <Link to='/perform'>Perform</Link>
                 <Link to='/contact'>Contact</Link>
