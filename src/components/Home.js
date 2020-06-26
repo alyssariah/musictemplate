bb698e05d65cd84226a80cee38d84c7775b4b483
Alyssa Jackson <alyssajackson@Alyssas-MacBook-Pro.local>
2020-06-25 18:48:59 -0600

    desktop images responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,7 @@ class Home extends React.Component {
                     <li>My mission is to provide students with the right tools, feedback, environment, and performance opportunities so they can enjoy and express themselves through music.</li>
                 </ul>
                 <img src="https://images.unsplash.com/photo-1579685055980-48dd748d862e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80" alt="hands playing piano"/>
-                <h6>Students have the opportunity to <Link to="/perform">perform</Link> at recitals and other unique settings such as nursing homes.</h6>
+                <h6 className="clearfix">Students have the opportunity to <Link to="/perform">perform</Link> at recitals and other unique settings such as nursing homes.</h6>
                 <h6><Link to="/lessons">Classes</Link> involve a blend of learning technical pieces, practicing theory, experimenting with improvisation, and customized musical material fit for their interests.</h6>
                 <h6>Please see the <Link to="/policies">student handbook</Link> to learn more about student policies.</h6>
                 <h6>Ready to sign up for classes?</h6>
